2d86b7ab18f784da26ce5c7dda7f9d389903b510
Arvin <yasir86@126.com>
2016-08-13 00:36:19 +0800

    modified AppDelegate.m

--- LaunchAnimation-demo/LaunchAnimation-demo/AppDelegate.m
+++ LaunchAnimation-demo/LaunchAnimation-demo/AppDelegate.m
@@ -19,10 +19,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    self.window.backgroundColor = [UIColor whiteColor];
-    UINavigationController *navigationVC = [[UINavigationController alloc]
-                                            initWithRootViewController:[[ViewController alloc] init]];
-    self.window.rootViewController = navigationVC;
+    [self.window setBackgroundColor:[UIColor whiteColor]];
+    [self.window setRootViewController:[[UINavigationController alloc]
+                                        initWithRootViewController:[[ViewController alloc] init]]];
     [self.window makeKeyAndVisible];
     [self.window addSubview:[[LaunchImageView alloc]
                              initWithFrame:self.window.bounds
